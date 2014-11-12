541402e3f1c448722804916642b78853cf1fdc86
akaramires <e.abdurayimov@gmail.com>
2014-11-12 21:00:37 +0400

    Init

---
+++ app/src/main/java/org/eatech/expense/MainActivity.java
@@ -0,0 +1,16 @@
+package org.eatech.expense;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+
+public class MainActivity extends SherlockActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
