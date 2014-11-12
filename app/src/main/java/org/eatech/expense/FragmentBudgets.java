7008be66d021914756aff9629f82d389b94afa15
akaramires <e.abdurayimov@gmail.com>
2014-11-12 23:55:05 +0400

    form layou with datepicker

--- app/src/main/java/org/eatech/expense/FragmentBudgets.java
+++ app/src/main/java/org/eatech/expense/FragmentBudgets.java
@@ -17,7 +17,7 @@ public class FragmentBudgets extends Fragment
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_form, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_common, container, false);
 
         return rootView;
     }
--- app/src/main/java/org/eatech/expense/FragmentForm.java
+++ app/src/main/java/org/eatech/expense/FragmentForm.java
@@ -5,20 +5,109 @@
 
 package org.eatech.expense;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class FragmentForm extends Fragment
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class FragmentForm extends SherlockFragment implements View.OnClickListener
 {
+    @InjectView(R.id.spinExpenseType)
+    Spinner spinExpenseType;
+
+    @InjectView(R.id.etDate)
+    EditText etDate;
+
+    private SimpleDateFormat dateFormatter;
+
+    private DatePickerDialog datePickerDialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.fragment_form, container, false);
+        ButterKnife.inject(this, rootView);
 
+        String[] data = getResources().getStringArray(R.array.lblExpenseTypes);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getSherlockActivity(), android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinExpenseType.setAdapter(adapter);
+        spinExpenseType.setPrompt(getString(R.string.lblExpenseType));
+        spinExpenseType.setSelection(0);
+
+        setHasOptionsMenu(true);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        etDate.setInputType(InputType.TYPE_NULL);
+        etDate.requestFocus();
+        etDate.setOnClickListener(this);
+        etDate.setText(dateFormatter.format(new Date().getTime()));
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(getSherlockActivity(), new DatePickerDialog.OnDateSetListener()
+        {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                etDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.menu_form, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                break;
+        }
+        return true;
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        datePickerDialog.show();
     }
 }
