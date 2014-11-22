a4b81ef3785a9fc4e43b010b91d2bc676151582f
akaramires <e.abdurayimov@gmail.com>
2014-11-22 22:12:39 +0300

    some changes

--- app/src/main/java/org/eatech/expense/fragments/FragmentOperations.java
+++ app/src/main/java/org/eatech/expense/fragments/FragmentOperations.java
@@ -6,20 +6,21 @@
 package org.eatech.expense.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.actionbarsherlock.app.SherlockListFragment;
+
 import org.eatech.expense.R;
 
-public class FragmentOperations extends Fragment
+public class FragmentOperations extends SherlockListFragment
 {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_common, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_operations_list, container, false);
 
         return rootView;
     }
